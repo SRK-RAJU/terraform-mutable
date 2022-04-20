fad386d3d46ca0affea94f0ee18b5d24e2e3a7fb
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-04-20 13:55:57 +0530

    updating

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -64,11 +64,13 @@ resource "aws_route_table" "public-route" {
       carrier_gateway_id           = ""
       "destination_prefix_list_id" = ""
       "egress_only_gateway_id"     = ""
-      "gateway_id"                 = aws_internet_gateway.igw.id
+      "gateway_id"                 = ""
+      #"gateway_id"                 = aws_internet_gateway.igw.id
       "instance_id"                = ""
       "ipv6_cidr_block"            = ""
       "local_gateway_id"           = ""
-      "nat_gateway_id"             = ""
+      #"nat_gateway_id"             = ""
+      "nat_gateway_id"             = aws_internet_gateway.igw.id
       "network_interface_id"       = ""
       "transit_gateway_id"         = ""
       "vpc_endpoint_id"            = ""
